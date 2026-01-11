75ab713f0176752027585ae8927c45faed324da8
ybchar <uiurihappy@naver.com>
2026-01-11 15:11:26 +0900

    initial Project

---
+++ src/main/java/com/hereeat/HereeatApplication.java
@@ -0,0 +1,13 @@
+package com.hereeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereeatApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HereeatApplication.class, args);
+	}
+
+}
