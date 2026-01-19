54823bd31e5fa1827c39ef981948b45deba823a5
강예린 <yerin7786@gmail.com>
2026-01-19 20:26:47 +0900

    [Feature] Gathering 도메인 정의 (#40)

---
+++ src/main/java/com/yogieat/domain/gathering/entity/GatheringEntity.java
@@ -0,0 +1,67 @@
+package com.yogieat.domain.gathering.entity;
+
+import com.yogieat.domain.gathering.domain.Gathering;
+import com.yogieat.domain.gathering.domain.value.Place;
+import com.yogieat.domain.gathering.domain.value.TimeSlot;
+import com.yogieat.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "t_gathering")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GatheringEntity extends BaseEntity {
+
+    @Column(name = "access_key")
+    private String accessKey;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "scheduled_date")
+    private LocalDate scheduledDate;
+
+    @Column(name = "time_slot")
+    private TimeSlot timeSlot;
+
+    @Column(name = "place")
+    private Place place;
+
+    @Column(name = "head_count")
+    private Integer headCount;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public GatheringEntity(
+            String accessKey,
+            String title,
+            LocalDate scheduledDate,
+            TimeSlot timeSlot,
+            Place place,
+            int headCount) {
+        this.accessKey = accessKey;
+        this.title = title;
+        this.scheduledDate = scheduledDate;
+        this.timeSlot = timeSlot;
+        this.place = place;
+        this.headCount = headCount;
+    }
+
+    public static Gathering toDomain(GatheringEntity entity) {
+        return new Gathering(
+                entity.getId(),
+                entity.getAccessKey(),
+                entity.getTitle(),
+                entity.getScheduledDate(),
+                entity.getTimeSlot(),
+                entity.getPlace(),
+                entity.getHeadCount()
+        );
+    }
+}
