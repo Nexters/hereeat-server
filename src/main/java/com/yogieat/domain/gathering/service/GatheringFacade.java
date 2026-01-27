9112bb39ff77838be512d923cef76a5d92d1b0a3
강예린 <yerin7786@gmail.com>
2026-01-27 20:10:19 +0900

    refactor: 모임 생성 시 Participant 자동 생성 로직 제거

--- src/main/java/com/yogieat/domain/gathering/service/GatheringFacade.java
+++ src/main/java/com/yogieat/domain/gathering/service/GatheringFacade.java
@@ -3,7 +3,6 @@ package com.yogieat.domain.gathering.service;
 import com.yogieat.domain.gathering.controller.request.CreateGatheringRequest;
 import com.yogieat.domain.gathering.controller.response.CreateGatheringResponse;
 import com.yogieat.domain.gathering.domain.Gathering;
-import com.yogieat.domain.participant.service.ParticipantService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,14 +12,10 @@ import org.springframework.stereotype.Service;
 public class GatheringFacade {
 
     private final GatheringService gatheringService;
-    private final ParticipantService participantService;
 
     @Transactional
     public CreateGatheringResponse createGathering(CreateGatheringRequest request) {
         Gathering gathering = gatheringService.create(request);
-        Long gatheringId = gathering.id();
-        participantService.createHostParticipant(request, gatheringId);
-
         return new CreateGatheringResponse(gathering.accessKey());
     }
 }
--- src/main/java/com/yogieat/domain/participant/service/ParticipantService.java
+++ src/main/java/com/yogieat/domain/participant/service/ParticipantService.java
@@ -1,10 +1,8 @@
 package com.yogieat.domain.participant.service;
 
-import com.yogieat.domain.gathering.controller.request.CreateGatheringRequest;
 import com.yogieat.domain.participant.domain.Participant;
 import com.yogieat.domain.participant.domain.value.DistanceRange;
 import com.yogieat.domain.participant.domain.value.Role;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,28 +27,6 @@ public class ParticipantService {
                 Role.MEMBER // 참여자는 기본적으로 MEMBER 역할
         );
         return participantRepository.save(participant);
-    }
-
-    /**
-     * 모임 생성자를 참여자로 등록
-     * - 설문 전 단계이므로 설문 관련 필드는 null 허용
-     *
-     * @param request 모임 생성 요청
-     * @param gatheringId 생성된 모임의 ID
-     */
-    @Transactional
-    public void createHostParticipant(CreateGatheringRequest request, Long gatheringId) {
-        Participant participant = new Participant(
-                null, // id는 저장 시 자동 생성
-                null, // 추후 인증 추가 시 userId로 설정
-                gatheringId,
-                null, // 설문 전
-                null, // 설문 전
-                null, // 설문 전
-                Role.MEMBER // 임시로 MEMBER로 저장
-        );
-
-        participantRepository.save(participant);
     }
 
     /**
