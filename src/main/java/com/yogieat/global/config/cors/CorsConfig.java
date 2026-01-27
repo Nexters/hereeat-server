522e61d1e88a0251ad2a65115f5569e96c5c6fc5
강예린 <yerin7786@gmail.com>
2026-01-28 02:31:40 +0900

    chore: local 및 dev 프론트 CORS 허용 설정 추가

---
+++ src/main/java/com/yogieat/global/config/cors/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.yogieat.global.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
+
+    public CorsConfig(CorsProperties corsProperties) {
+        this.corsProperties = corsProperties;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins())
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
