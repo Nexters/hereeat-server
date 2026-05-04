c663d782a99a56484986f8f63b58c3f00296b07d
char-yb <uiurihappy@naver.com>
2026-05-04 13:02:23 +0900

    [Fix] Swagger 서버 URL HTTPS 적용

--- support/swagger/src/main/java/com/yogieat/swagger/SwaggerConfig.java
+++ support/swagger/src/main/java/com/yogieat/swagger/SwaggerConfig.java
@@ -2,15 +2,23 @@ package com.yogieat.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private final SwaggerProperties swaggerProperties;
+
+    public SwaggerConfig(SwaggerProperties swaggerProperties) {
+        this.swaggerProperties = swaggerProperties;
+    }
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url(swaggerProperties.serverUrl())))
                 .info(new Info()
                         .title("Yogieat API")
                         .description("Yogieat Server API Documentation")
