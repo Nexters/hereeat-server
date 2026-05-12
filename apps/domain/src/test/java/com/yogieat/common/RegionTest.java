60316e64aa856d4626090e38dd1c7dfe926a51fb
char-yb <uiurihappy@naver.com>
2026-05-12 15:52:39 +0900

    [Fix] Region 하드코딩 제거

--- apps/domain/src/test/java/com/yogieat/common/RegionTest.java
+++ apps/domain/src/test/java/com/yogieat/common/RegionTest.java
@@ -7,19 +7,10 @@ import org.junit.jupiter.api.Test;
 class RegionTest {
 
     @Test
-    void fromString_shouldResolveDefaultRegionMetadata() {
+    void fromString_shouldCreateCodeOnlyRegion() {
         Region region = Region.fromString("gangnam");
 
         assertThat(region.name()).isEqualTo("GANGNAM");
-        assertThat(region.getName()).isEqualTo("강남역");
-        assertThat(region.getCoordinatesStandard().getCoordinates()).containsExactly(127.0276, 37.4979);
-    }
-
-    @Test
-    void fromString_shouldKeepUnknownRegionAsCodeOnly() {
-        Region region = Region.fromString("custom");
-
-        assertThat(region.name()).isEqualTo("CUSTOM");
         assertThat(region.getName()).isNull();
         assertThat(region.getCoordinatesStandard()).isNull();
     }
