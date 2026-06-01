b963c0d96c05d77e4afedac7bd82404d0ad0e9d9
강예린 <yerin7786@gmail.com>
2026-06-02 00:23:45 +0900

    refactor: ST_Distance를 ST_DWithin으로 변경

--- storage/db-core/src/main/resources/db/migration/V10__hide_restaurants_out_of_700m_radius.sql
+++ storage/db-core/src/main/resources/db/migration/V10__hide_restaurants_out_of_700m_radius.sql
@@ -5,7 +5,8 @@ FROM t_region reg
 WHERE r.region_id = reg.id
   AND r.deleted_at IS NULL
   AND r.is_display = true
-  AND ST_Distance(
+  AND NOT ST_DWithin(
           r.location::geography,
-          ST_SetSRID(ST_MakePoint(reg.longitude, reg.latitude), 4326)::geography
-      ) > 700;
+          ST_SetSRID(ST_MakePoint(reg.longitude, reg.latitude), 4326)::geography,
+          700
+      );
